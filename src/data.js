9bf6e79cf1ab83c7af84c190e5a591760ff612e9
NICOLAS ARAUJO <nicolasfcaraujo@gmail.com>
2023-03-19 15:19:31 -0300

    galleryUpdate

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 /*MENU*/
-import img1 from "./images/img-1.jpeg"
+import img1 from "./images/img-1.jpg"
 import img12 from "./images/img-1-2.jpg"
 import img2 from "./images/img-2.jpg"
 import img22 from "./images/img-2-2.jpg"
@@ -13,17 +13,18 @@ import img6 from "./images/img-6.jpg"
 import img62 from "./images/img-6-2.jpg"
 import img7 from "./images/img-7.jpg"
 import img72 from "./images/img-7-2.jpg"
+import img8 from "./images/img-8.jpg"
 /*GALLERY*/
 import g1 from "./images/g1.jpg"
 import g2 from "./images/g2.jpg"
 import g3 from "./images/g3.jpg"
-import g4 from "./images/g4.png"
+import g4 from "./images/g4.jpg"
 import g5 from "./images/g5.jpg"
 import g6 from "./images/g6.jpg"
 import g7 from "./images/g7.jpg"
 import g8 from "./images/g8.jpg"
 import g9 from "./images/g9.jpg"
-import g10 from "./images/g10.png"
+import g10 from "./images/g10.jpg"
 import g11 from "./images/g11.jpg"
 import g12 from "./images/g12.jpg"
 import g13 from "./images/g13.jpg"
@@ -34,6 +35,12 @@ import g17 from "./images/g17.jpg"
 import g18 from "./images/g18.jpg"
 import g19 from "./images/g19.jpg"
 import g20 from "./images/g20.jpg"
+import g21 from "./images/g21.jpg"
+import g22 from "./images/g22.jpg"
+import g23 from "./images/g23.jpg"
+import g24 from "./images/g24.jpg"
+import g25 from "./images/g25.jpg"
+
 
 export const slideImgs = [
     {
@@ -70,6 +77,11 @@ export const slideImgs = [
         id: 7,
         name: "cyber girl face",
         image: img7,
+    },
+    {
+        id: 8,
+        name: "cyber girl face",
+        image: img8,
     },
 ]
 
@@ -108,6 +120,11 @@ export const menuImagesSmall = [
         id: 7,
         name: "cyber girl face",
         image: img72,
+    },
+    {
+        id: 8,
+        name: "cyber girl face",
+        image: img8,
     },
 ]
 
@@ -214,5 +231,25 @@ export const galleryImages = [
     {
         id: 20,
         url: g20,
+    },
+    {
+        id: 21,
+        url: g21,
+    },
+    {
+        id: 22,
+        url: g22,
+    },
+    {
+        id: 23,
+        url: g23,
+    },
+    {
+        id: 24,
+        url: g24,
+    },
+    {
+        id: 25,
+        url: g25,
     },
 ]
