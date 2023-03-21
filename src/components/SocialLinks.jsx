0a44b79e61cac2cf2dc9d880ebf2bb8dd771be71
NICOLAS ARAUJO <nicolasfcaraujo@gmail.com>
2023-03-21 10:01:15 -0300

    socialLinksSpace

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -6,7 +6,7 @@ const SocialLinks = ({justify="flex-end"}) => {
             {socialLinks.map((link) => {
                 const {id, className, url} = link
                 return (
-                    <li key={id} style={{paddingRight: "20px"}}>
+                    <li key={id} style={{paddingRight: "40px"}}>
                         <a href={url} style={{textDecoration: "none"}}>
                             <i className={className} style={{color: "#7a7a7a", fontSize: "1.2rem"}}></i>
                         </a>
